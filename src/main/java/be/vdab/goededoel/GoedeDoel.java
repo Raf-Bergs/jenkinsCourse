87ac78c36a3ef8ace80e9a90aefb823d8f2b10d5
raf-bergs <r0752792@student.thomasmore.be>
2025-04-04 13:59:44 +0200

    hashcode test

--- src/main/java/be/vdab/goededoel/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/GoedeDoel.java
@@ -23,4 +23,9 @@ public class GoedeDoel {
     public boolean equals(Object object) {
         return object instanceof GoedeDoel ander && naam.equalsIgnoreCase(ander.naam);
     }
+
+    @Override
+    public int hashCode() {
+        return this.naam.toUpperCase().hashCode();
+    }
 }
